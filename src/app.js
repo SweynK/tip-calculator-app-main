8eb5d807ba8e41b5bc07dcab3ec567eb9cdc7470
Gkvesiashvili <kveska98@gmail.com>
2024-01-28 21:32:25 +0400

    add input validation

--- src/app.js
+++ src/app.js
@@ -12,19 +12,39 @@ let peopleValue = 0;
 
 billInput.addEventListener("input", (e) => {
   billValue = Number(e.target.value);
-  calculation();
+  if (billValue > 0) {
+    e.target.classList.remove("border-red-400");
+    e.target.classList.add("border-green-400");
+    e.target.classList.add("border-2");
+    calculation();
+  } else {
+    e.target.classList.remove("border-green-400");
+    e.target.classList.add("border-red-400");
+  }
 });
 
 tipInput.forEach((el) => {
   el.addEventListener("click", (e) => {
     tipValue = parseInt(e.target.innerText);
     calculation();
+    tipInput.forEach((el) => {
+      el.classList.remove("bg-[#26C2AE]");
+    });
+    el.classList.add("bg-[#26C2AE]");
   });
 });
 
 peopleInput.addEventListener("input", (e) => {
   peopleValue = Number(e.target.value);
-  calculation();
+  if (peopleValue > 0) {
+    e.target.classList.remove("border-red-400");
+    e.target.classList.add("border-green-400");
+    e.target.classList.add("border-2");
+    calculation();
+  } else {
+    e.target.classList.remove("border-green-400");
+    e.target.classList.add("border-red-400");
+  }
 });
 
 function calculation() {
@@ -44,14 +64,25 @@ customInput.addEventListener("input", (e) => {
   tipValue = Number(e.target.value);
   calculation();
 });
+customInput.addEventListener("click", () => {
+  tipInput.forEach((el) => {
+    el.classList.remove("bg-[#26C2AE]");
+  });
+});
 
 btnReset.addEventListener("click", () => {
   billValue = 0;
   tipValue = 0;
   peopleValue = 0;
-  customInput.value = "Custom";
+  customInput.value = "";
   resultTip.innerText = "$0.00";
   resultTotal.innerText = "$0.00";
   billInput.value = "";
   peopleInput.value = "";
+  billInput.classList.remove("border-red-400");
+  billInput.classList.remove("border-green-400");
+  billInput.classList.remove("border-2");
+  peopleInput.classList.remove("border-red-400");
+  peopleInput.classList.remove("border-green-400");
+  peopleInput.classList.remove("border-2");
 });
